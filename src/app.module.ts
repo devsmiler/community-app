662e25ec0ff6630db2ea97028dc86becffaaa53b
Minkyu Kim <minkyukim@Minkyuui-MacBookPro.local>
2022-05-12 11:13:06 +0900

    change config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
